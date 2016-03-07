e8f205421e54f10ab0b5d97efed87d85bbc059a3
Vaggelis Nikolopoulos <nikolopoulos.vaggelis@gmail.com>
2016-03-07 21:50:11 +0200

    bootstrap.sh update

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,6 @@
 
 echo $(hostname --ip-address) > ./etc/exareme/master
 ./bin/exareme-admin.sh --start --local
-
 if [[ -e "./var/log/exareme-master.log" ]]; then
 
     tail -f ./var/log/exareme-master.log
